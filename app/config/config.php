8b141f330603e0cf9dc74b79c8170f6343e5bf48
zlsh09830123 <zlsh09830123@gmail.com>
2020-09-10 22:12:11 +0800

    Config file & Uploader completed

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'http://videotube.gq:8002');
+    // Site Name
+    define('SITENAME', 'OOP_MVC');
