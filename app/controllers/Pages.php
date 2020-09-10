a4179daa34cbc667066c3523b949355885de9e9c
zlsh09830123 <zlsh09830123@gmail.com>
2020-09-10 21:15:06 +0800

    Loading views completed

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,10 +5,14 @@
         }
 
         public function index() {
-            $this->view('hello');
+            $data = [
+                'title' => 'Welcome'
+            ];
+
+            $this->view('pages/index', $data);
         }
 
-        public function about($id) {
-            echo $id;
+        public function about() {
+            $this->view('pages/about');
         }
     }
