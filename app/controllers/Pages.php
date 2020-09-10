23a4e0de92e101705a7edcbd73ee641157cc8bbf
zlsh09830123 <zlsh09830123@gmail.com>
2020-09-10 20:48:14 +0800

    Base controller class completed

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,11 +1,11 @@
 <?php
-    class Pages {
+    class Pages extends Controller {
         public function __construct() {
             
         }
 
         public function index() {
-            
+            $this->view('hello');
         }
 
         public function about($id) {
