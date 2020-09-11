1d3f4726e529159d12a2f535d9efe08cc8e420f9
zlsh09830123 <zlsh09830123@gmail.com>
2020-09-11 16:38:05 +0800

    TraversyMVC PHP framework

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,13 @@
 <?php
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', '_YOUR_USER_');
+    define('DB_PASS', '_YOUR_PASS_');
+    define('DB_NAME', '_YOUR_DBNAME_');
+
     // App Root
     define('APPROOT', dirname(dirname(__FILE__)));
     // URL Root
-    define('URLROOT', 'http://videotube.gq:8002');
+    define('URLROOT', '_YOUR_URL_');
     // Site Name
-    define('SITENAME', 'OOP_MVC');
+    define('SITENAME', '_YOUR_SITENAME_');
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -6,7 +6,7 @@
 
         public function index() {
             $data = [
-                'title' => 'Welcome'
+                'title' => 'TraversyMVC'
             ];
 
             $this->view('pages/index', $data);
